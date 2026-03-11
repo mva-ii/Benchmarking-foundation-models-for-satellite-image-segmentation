198735c4b35a6db6aee6ce8e96cbae9b139a10c8
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2026-03-11 12:15:28 +0100

    chore: reduce number of gpus

--- slurm/fit.sh
+++ slurm/fit.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=gpu_test
 #SBATCH --nodes=4             # This needs to match Trainer(num_nodes=...)
 #SBATCH --gres=gpu:2
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=4   # This needs to match Trainer(devices=...)
 #SBATCH --mem=128G
 #SBATCH --time=00:10:00
