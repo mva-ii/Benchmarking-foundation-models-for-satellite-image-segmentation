7fdc0fa7968df1984d64d3d5891d2a4e13f01f7c
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2026-03-11 12:31:11 +0100

    chore: update job submission script and configuration for training on Tessera Ruche cluster

--- slurm/fit.sh
+++ slurm/fit.sh
@@ -23,6 +23,11 @@ export CUDA_LAUNCH_BLOCKING=1
 
 # might need the latest CUDA
 # module load NCCL/2.4.7-1-cuda.10.0
-source .venv/bin/activate
+
+source $ENVIRONMENT_ROOT/bin/activate
+
 # run script from above
-srun python -c $CONFIG_FILE --data.init_args.pastis_r_root=$PASTIS_R_ROOT --data.init_args.embedding_root=$EMBEDDING_ROOT
+srun python scripts/runme.py \
+-c $CONFIG_FILE \
+--data.init_args.pastis_r_root=$PASTIS_R_ROOT \
+--data.init_args.embedding_root=$EMBEDDING_ROOT
