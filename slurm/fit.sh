cdc9afd98b0831d093dd7760ea4dd6bc1325791e
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2026-03-11 11:57:31 +0100

    feat: add slurm script

---
+++ slurm/fit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+# (submit.sh)
+
+# SLURM SUBMIT SCRIPT
+#SBATCH --nodes=4             # This needs to match Trainer(num_nodes=...)
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8   # This needs to match Trainer(devices=...)
+#SBATCH --mem=0
+#SBATCH --time=0-02:00:00
+
+# debugging flags (optional)
+export NCCL_DEBUG=INFO
+export PYTHONFAULTHANDLER=1
+export CUDA_LAUNCH_BLOCKING=1
+
+# on your cluster you might need these:
+# set the network interface
+# export NCCL_SOCKET_IFNAME=^docker0,lo
+
+# might need the latest CUDA
+# module load NCCL/2.4.7-1-cuda.10.0
+
+# run script from above
+srun uv run scripts/runme.py -c $CONFIG_FILE --data.init_args.pastis_r_root=$PASTIS_R_ROOT --data.init_args.embedding_root=$EMBEDDING_ROOT
