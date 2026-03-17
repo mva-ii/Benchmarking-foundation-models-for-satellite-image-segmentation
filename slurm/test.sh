8bed9061b7a4d6154a50ff848387fa435b8af11d
Simon Sassi <12765875+maxerbox@users.noreply.github.com>
2026-03-17 11:43:37 +0100

    revert: put back multiple device as it is blocking with sync_dist

--- slurm/test.sh
+++ slurm/test.sh
@@ -4,9 +4,9 @@
 # SLURM SUBMIT SCRIPT
 #SBATCH --partition=gpu_test
 #SBATCH --nodes=1             # This needs to match Trainer(num_nodes=...)
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=10
-#SBATCH --ntasks-per-node=1   # This needs to match Trainer(devices=...)
+#SBATCH --ntasks-per-node=4   # This needs to match Trainer(devices=...)
 #SBATCH --mem=128G
 #SBATCH --time=01:00:00
 #SBATCH --error=%j.out
